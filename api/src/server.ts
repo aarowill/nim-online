37e8629db5fb6f1e4d7d67308e2484c1b20b2029
Aaron Williamson <guitarfanman@gmail.com>
2020-10-21 00:43:14 -0600

    Set up initial boilerplate

---
+++ api/src/server.ts
@@ -0,0 +1,49 @@
+import morgan from 'morgan';
+import path from 'path';
+import helmet from 'helmet';
+
+import express, { Request, Response } from 'express';
+import StatusCodes from 'http-status-codes';
+import 'express-async-errors';
+
+import logger from '@shared/logger';
+import BaseRouter from './routes';
+
+const app = express();
+const { BAD_REQUEST } = StatusCodes;
+
+// Set basic express settings
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Show routes called in console during development
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Security
+app.use(helmet());
+
+// Add APIs
+app.use('/api', BaseRouter);
+
+// Print API errors
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: Error, _req: Request, res: Response) => {
+  logger.err(err, true);
+  return res.status(BAD_REQUEST).json({
+    error: err.message
+  });
+});
+
+// Serve front-end content
+const viewsDir = path.join(__dirname, 'views');
+app.set('views', viewsDir);
+const staticDir = path.join(__dirname, 'public');
+app.use(express.static(staticDir));
+app.get('*', (_req: Request, res: Response) => {
+  res.sendFile('index.html', { root: viewsDir });
+});
+
+// Export express instance
+export default app;
