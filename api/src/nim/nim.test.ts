d4a277dae5a92a86f960adb93b52b0d444d9a5de
Aaron Williamson <guitarfanman@gmail.com>
2020-10-22 18:39:58 -0600

    Finish Nim game logic and tests

--- api/src/nim/nim.test.ts
+++ api/src/nim/nim.test.ts
@@ -1,4 +1,4 @@
-import { newGameState } from './nim';
+import { newGameState, doTurn, NimGame } from './nim';
 
 describe('nim', () => {
   describe('newGameState', () => {
@@ -8,7 +8,8 @@ describe('nim', () => {
       expect(game.currentPlayerTurn).toBe(0);
       expect(game.remainingSticks).toBe(20);
       expect(game.maxPickupPerTurn).toBe(3);
-      expect(game.lastStickOnTurnLoses).toBe(true);
+      expect(game.lastStickOnTurnLoses).toEqual(true);
+      expect(game.winner).toBeUndefined();
     });
 
     describe('when configured', () => {
@@ -28,12 +29,28 @@ describe('nim', () => {
         expect(game.remainingSticks).toEqual(expectedNumberOfSticks);
       });
 
+      it('fails to create a board with too many sticks', () => {
+        expect(() => newGameState({ numberOfSticks: 31 })).toThrow(/too many sticks/i);
+      });
+
+      it('fails to create a board with too few sticks', () => {
+        expect(() => newGameState({ numberOfSticks: 3 })).toThrow(/too few sticks/i);
+      });
+
       it('sets the maximum pickup per turn', () => {
-        const expectedPickupPerTurn = 10;
+        const expectedPickupPerTurn = 5;
 
         const game = newGameState({ maxPickupPerTurn: expectedPickupPerTurn });
 
         expect(game.maxPickupPerTurn).toEqual(expectedPickupPerTurn);
+      });
+
+      it('fails to create a board with too high max pickup', () => {
+        expect(() => newGameState({ maxPickupPerTurn: 6 })).toThrow(/too high/i);
+      });
+
+      it('fails to create a board with too low max pickup', () => {
+        expect(() => newGameState({ maxPickupPerTurn: 1 })).toThrow(/too low/i);
       });
 
       it('sets whether the last stick on player turn loses', () => {
@@ -42,6 +59,93 @@ describe('nim', () => {
         const game = newGameState({ lastStickOnTurnLoses: expectedLastStickLoses });
 
         expect(game.lastStickOnTurnLoses).toEqual(expectedLastStickLoses);
+      });
+    });
+
+    describe('doTurn', () => {
+      let game: NimGame;
+
+      beforeEach(() => {
+        game = newGameState();
+      });
+
+      it('updates the stick count and player turn', () => {
+        const newGame = doTurn(game, 3);
+
+        expect(newGame.remainingSticks).toEqual(game.remainingSticks - 3);
+        expect(newGame.currentPlayerTurn).toEqual(1);
+      });
+
+      it('fails when too many sticks are taken', () => {
+        expect(() => doTurn(game, 4)).toThrow(/too many/i);
+      });
+
+      it('fails when too few sticks are taken', () => {
+        expect(() => doTurn(game, 0)).toThrow(/too few/i);
+      });
+
+      it('fails when trying to take more sticks than remain in the game', () => {
+        game.remainingSticks = 2;
+
+        expect(() => doTurn(game, 3)).toThrow(/not enough sticks remaining/i);
+      });
+
+      describe('on the last turn', () => {
+        beforeEach(() => {
+          game.remainingSticks = 4;
+        });
+
+        describe('when there is one stick left', () => {
+          it('identifies the end of the game', () => {
+            const newGame = doTurn(game, 3);
+
+            expect(newGame.winner).not.toBeUndefined();
+          });
+
+          it('identifies the winner of the game', () => {
+            game.currentPlayerTurn = 0;
+            game.lastStickOnTurnLoses = true;
+            const newGame = doTurn(game, 3);
+
+            expect(newGame.winner).toEqual(0);
+          });
+
+          it('identifies the winner of a alternate rules game', () => {
+            game.lastStickOnTurnLoses = false;
+            game.currentPlayerTurn = 0;
+            const newGame = doTurn(game, 3);
+
+            expect(newGame.winner).toEqual(1);
+          });
+        });
+
+        describe('when there are zero sticks left', () => {
+          beforeEach(() => {
+            game.remainingSticks = 3;
+          });
+
+          it('identifies the end of the game', () => {
+            const newGame = doTurn(game, 3);
+
+            expect(newGame.winner).not.toBeUndefined();
+          });
+
+          it('identifies the winner of the game', () => {
+            game.currentPlayerTurn = 0;
+            game.lastStickOnTurnLoses = true;
+            const newGame = doTurn(game, 3);
+
+            expect(newGame.winner).toEqual(1);
+          });
+
+          it('identifies the winner of a alternate rules game', () => {
+            game.lastStickOnTurnLoses = false;
+            game.currentPlayerTurn = 0;
+            const newGame = doTurn(game, 3);
+
+            expect(newGame.winner).toEqual(0);
+          });
+        });
       });
     });
   });
--- api/src/nim/nim.ts
+++ api/src/nim/nim.ts
@@ -6,14 +6,27 @@ export interface NimGame {
   remainingSticks: number;
   maxPickupPerTurn: number;
   lastStickOnTurnLoses: boolean;
+  winner: 0 | 1 | undefined;
 }
 
+/**
+ * Options for a new game of Nim.
+ */
 interface NimOptions {
   firstTurn?: 0 | 1;
   numberOfSticks?: number;
   maxPickupPerTurn?: number;
   lastStickOnTurnLoses?: boolean;
 }
+
+/**
+ * Get the next player given a current player.
+ *
+ * @returns the next player
+ */
+export const nextPlayer = (currentPlayer: 0 | 1): 0 | 1 => {
+  return (1 - currentPlayer) as 0 | 1;
+};
 
 /**
  * Create a new game state with optional configuration.
@@ -25,13 +38,63 @@ export const newGameState = (
     firstTurn: 0,
     numberOfSticks: 20,
     maxPickupPerTurn: 3,
-    lastStickOnTurnLoses: true
+    lastStickOnTurnLoses: true,
   }
 ): NimGame => {
+  if (numberOfSticks > 30) {
+    throw new Error('Too many sticks. Max is 30');
+  }
+
+  if (numberOfSticks < 5) {
+    throw new Error('Too few sticks. Min is 5');
+  }
+
+  if (maxPickupPerTurn > 5) {
+    throw new Error('Max pickup per turn is too high. Max is 5');
+  }
+
+  if (maxPickupPerTurn < 2) {
+    throw new Error('Max pickup per turn is too low. Min is 2');
+  }
+
   return {
     currentPlayerTurn: firstToMove,
     remainingSticks: numberOfSticks,
     maxPickupPerTurn,
-    lastStickOnTurnLoses
+    lastStickOnTurnLoses,
+    winner: undefined,
   };
+};
+
+/**
+ * Perform a turn of the game.
+ *
+ * @returns a new game state
+ */
+export const doTurn = (gameState: NimGame, numberOfSticks: number): NimGame => {
+  const newGame = { ...gameState };
+
+  if (numberOfSticks < 1) {
+    throw new Error('Tried to take too few sticks. Min is 1');
+  }
+
+  if (numberOfSticks > gameState.maxPickupPerTurn) {
+    throw new Error(`Tried to take too many sticks. Max is ${gameState.maxPickupPerTurn}`);
+  }
+
+  if (numberOfSticks > gameState.remainingSticks) {
+    throw new Error(`Not enough sticks remaining. Only ${gameState.remainingSticks} left`);
+  }
+
+  newGame.remainingSticks -= numberOfSticks;
+
+  if (newGame.remainingSticks === 0) {
+    newGame.winner = newGame.lastStickOnTurnLoses ? nextPlayer(newGame.currentPlayerTurn) : newGame.currentPlayerTurn;
+  } else if (newGame.remainingSticks === 1) {
+    newGame.winner = newGame.lastStickOnTurnLoses ? newGame.currentPlayerTurn : nextPlayer(newGame.currentPlayerTurn);
+  }
+
+  newGame.currentPlayerTurn = nextPlayer(newGame.currentPlayerTurn);
+
+  return newGame;
 };
