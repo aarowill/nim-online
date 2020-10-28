a85221378c59aae976d9cf264fe5d80c75f0feac
Aaron Williamson <guitarfanman@gmail.com>
2020-10-27 22:20:29 -0600

    Update to build image

---
+++ client/build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+echo Building aarowill/nim-caddy:build
+
+docker build -t aarowill/nim-caddy:build . -f Dockerfile.build
+
+docker container create --name extract aarowill/nim-caddy:build
+docker container cp extract:/app/build ./build
+docker container rm -f extract
+
+echo Building aarowill/nim-caddy:latest
+
+docker build -t aarowill/nim-caddy:latest .
