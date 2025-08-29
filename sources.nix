5a8161c3c70637af04bda00dddf22fb9a22e3da5
Aaron Williamson <guitarfanman@gmail.com>
2025-08-28 19:24:24 -0600

    Update nix pin

--- sources.nix
+++ sources.nix
@@ -1,8 +1,8 @@
 {
   # https://status.nixos.org/
   nixpkgs = fetchTarball {
-    name = "nixos-25.05-2025-07-21";
-    url = "https://github.com/NixOS/nixpkgs/archive/f01fe91b0108a7aff99c99f2e9abbc45db0adc2a.tar.gz";
-    sha256 = "1wx7rx66prphrsizqb5ivcj5l86bv3gy47vl8fcqk1gnnzc14pim";
+    name = "nixos-25.05-2025-08-28";
+    url = "https://github.com/NixOS/nixpkgs/archive/4e7667a90c167f7a81d906e5a75cba4ad8bee620.tar.gz";
+    sha256 = "1spjsasb7krm1063sxy93mjv0jq09kdqh3s7xq479nzr4dym4zac";
   };
 }
