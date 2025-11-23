db64af3bb324c88bfdfd256fafa96ca3834f7f96
Aaron Williamson <guitarfanman@gmail.com>
2025-11-23 12:18:30 -0700

    Update SSH key and nix pin

--- modules/systemConfig.nix
+++ modules/systemConfig.nix
@@ -12,8 +12,8 @@
         isNormalUser = true;
         extraGroups = [ "wheel" ];
         openssh.authorizedKeys.keys = [
-          # MBP
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIk4ZcfPqqykrkiKZ0EsLyQCZ1pHpUpnODik8uZOTc7J ssh@aarowill.ca"
+          # Main key
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGiFaoFW9+h972eJBgKNZur94At5hIpootcBmGKzyojw ssh@aarowill.ca"
         ];
         hashedPassword = "$6$wdswmUrNpL$UjNAEVmcbnnQ6AI38JZJ2U2U3RVy.9BWFuPov1EfLm0uV9bq5iRUxd5oz7wrDzb30tQnFPy26F/9Ms7nhY93L/";
       };
--- sources.nix
+++ sources.nix
@@ -1,8 +1,8 @@
 {
   # https://status.nixos.org/
   nixpkgs = fetchTarball {
-    name = "nixos-25.05-2025-10-10";
-    url = "https://github.com/NixOS/nixpkgs/archive/5da4a26309e796daa7ffca72df93dbe53b8164c7.tar.gz";
-    sha256 = "1v7cpghxf5rl1l0rj32n89jv5w7z34lld0kiai8kd50s97fvw8n1";
+    name = "nixos-25.05-2025-11-23";
+    url = "https://github.com/NixOS/nixpkgs/archive/c58bc7f5459328e4afac201c5c4feb7c818d604b.tar.gz";
+    sha256 = "1lypgq8f1v571vgrlqj13yi97bhlw9gsqc0s9di5mn5w5fxjfd6n";
   };
 }
